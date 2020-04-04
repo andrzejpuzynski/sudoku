832d340bb5f87214d275e0fd3a04b21707fc9aae
Andrzej Puzynski <andrzejpuzynski@tlen.pl>
2020-04-04 10:51:11 +0200

    Using Backtracking algorithm to solve Sudoku

--- src/index.js
+++ src/index.js
@@ -1,150 +1,74 @@
 module.exports = function solveSudoku(matrix) {
-  var inputArray = matrix
   
-  var outputArray = [];
+  var inputArray = matrix;
+  var outputArray = inputArray;
 
+  // --------------------------------   check if n not in particular row
+  const usedInRow = (row, n, array) => {
+    for (i = 0; i < 9; i++) {
+      if (array[row][i] == n) {
+        return true;
+      }
+    }
+    return false;
+  }
 
-  //  1 STEP OF ALGORITHM: CHANGING ZEROES INTO ALL POSSIBLE VALUES
-  console.clear();
-  
-  const checkCell = (row, column, array) => {
-    var outputCheckedCell = [];
-  
-    var arrayRow = array[row];
-    var arrayColumn = array.map((i, idx) => (i = i[column]));
-  
-    function validateSection(row, col, array) {
-      var arraySection = [];
-      var xrow = Math.floor(row / 3) * 3;
-      var xcol = Math.floor(col / 3) * 3;
-  
-      for (var i = 0; i < 3; i++) {
-        for (var j = 0; j < 3; j++) {
-          arraySection.push(array[xrow + i][xcol + j]);
+  // -------------------------------   check if n not in particular column
+  const usedInColumn = (col, n, array) => {
+    for (i = 0; i < 9; i++) {
+      if (array[i][col] == n) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // -------------------------------   check if n not in particular box
+  const usedInBox = (boxStartRow, boxStartCol, n, array) => {
+    for (i = 0; i < 3; i++) {
+      for (j = 0; j < 3; j++) {
+        if (array[i + boxStartRow][j + boxStartCol] == n) {
+          return true;
         }
       }
-      return arraySection;
     }
-  
-    arraySection = validateSection(row, column, array);
-  
-    for (var r = 1; r < 10; r++) {
-      if (
-        !arrayRow.includes(r) &&
-        !arrayColumn.includes(r) &&
-        !arraySection.includes(r)
-      ) {
-        outputCheckedCell.push(r);
-      }
-    }
-    return outputCheckedCell.length === 1
-      ? outputCheckedCell[0]
-      : outputCheckedCell;
-  };
-  
-  // function change zeros onto possible values
-  for (i = 0; i < 9; i++) {
-    outputArray.push([]);
-    inputArray[i].map((item, idx) => {
-      inputArray[i][idx] != 0
-        ? outputArray[i].push(inputArray[i][idx])
-        : outputArray[i].push(checkCell(i, idx, inputArray));
-    });
+    return false;
   }
-  
-  
-  // 2: REDUCING ELEMENTS IN ROW 
-  
-  const filteredAndFlatenRow = (row, column, array) => {
-    return array[row].filter((x, idx) => idx != column)
-                     .reduce((a, b) => a.concat(b), []);
-  };
-  
-  function reduceRow(array) {
-    for (i = 0; i < 9; i++) {
-      for (j = 0; j < 9; j++) {
-        array[i][j].length > 1
-          ? array[i][j].map((item) =>
-              filteredAndFlatenRow(i, j, array).includes(item)
-                ? null
-                : outputArray[i][j] = item
-            )
-          : null;
-      }
-    }
-  }
-  
-  // 3: REDUCING ELEMENTS IN COLUMN
 
-  const filteredAndFlatenColumn = (row, column, array) => {
-    return array.filter((x, idx) => idx != column)
-                .map(x => x = x[column])
-                .reduce((a, b) => a.concat(b), []);
-  };
-  
-  
-  function reduceColumn(array) {
-    for (i = 0; i < 9; i++) {
-      for (j = 0; j < 9; j++) {
-        array[i][j].length > 1
-          ? array[i][j].map((item) => 
-              filteredAndFlatenColumn(i, j, array).includes(item)
-                ? null
-                : outputArray[i][j] = item
-            )
-          : null;
-      }  
+  // --------------------------------  check if n can be placed in cell
+  const isSafe = (row, col, n, array) => {
+    if (!usedInRow(row, n, array) &&
+      !usedInColumn(col, n, array) &&
+      !usedInBox(row - row % 3, col - col % 3, n, array)) {
+      return true;
     }
+    return false;
   }
-  
-  
-  // 4: REDUCING ELEMENTS IN COLUMN
-  
-  const filteredAndFlatenSection = (row, column, array) => {
-      var filteredArray = [];
-      var xrow = Math.floor(row / 3) * 3;
-      var xcol = Math.floor(column / 3) * 3;
-  
-      for (var i = 0; i < 3; i++) {
-        for (var j = 0; j < 3; j++) {
-          !((xrow + i == row) && (xcol + j == column)) ?
-          filteredArray.push(array[xrow + i][xcol + j]) :
-          null
+
+  // --------------------------------  solve Sudoku
+  const solveSudoku = (array) => {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (array[i][j] == 0) {
+          for (let k = 1; k <= 9; k++) {
+            if (isSafe(i, j, k, array)) {
+              array[i][j] = k;
+              if (solveSudoku(array)) {
+                return true;
+              } else {
+                array[i][j] = 0;
+              }
+            }
+          }
+          return false;
         }
       }
-    return filteredArray.reduce((a, b) => a.concat(b), [])
-  };
-  
-  function reduceSection(array) {
-    for (i = 0; i < 9; i++) {
-      for (j = 0; j < 9; j++) {
-        array[i][j].length > 1
-          ? array[i][j].map((item) => 
-              filteredAndFlatenSection(i, j, array).includes(item)
-                ? null
-                : outputArray[i][j] = item          
-            )
-         : null;
-      }  
     }
+    return true;
   }
-  
-  // 5; RUNNING SECOND ALGORITHM
-  reduceRow(outputArray);
-  reduceColumn(outputArray);
-  reduceSection(outputArray);
-  reduceRow(outputArray);
-  reduceColumn(outputArray);
-  reduceSection(outputArray);
-  reduceRow(outputArray);
-  reduceColumn(outputArray);
-  reduceSection(outputArray);
-  reduceRow(outputArray);
-  reduceColumn(outputArray);
-  reduceSection(outputArray);
-  reduceRow(outputArray);
-  reduceColumn(outputArray);
-  reduceSection(outputArray);
 
-  return outputArray
+// --------------------------------  run
+  solveSudoku(inputArray);
+  return outputArray;
+
 }
